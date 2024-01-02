771e2636c2131778ffe8c88f6fd5e70632410aae
kmentz <kassiane.mentz@adp.com>
2024-01-02 19:41:21 -0300

    iniciando o projeto

---
+++ tech_challenge_web_streaming/src/main/java/com/fiap/tech_challenge_web_streaming/TechChallengeWebStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.tech_challenge_web_streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeWebStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeWebStreamingApplication.class, args);
+	}
+
+}
---
+++ tech_challenge_web_streaming/src/test/java/com/fiap/tech_challenge_web_streaming/TechChallengeWebStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.tech_challenge_web_streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeWebStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
