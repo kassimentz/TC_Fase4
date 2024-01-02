f290ce49686d0e2f8f4c31762d7b855f617a1c5b
kmentz <kassiane.mentz@adp.com>
2024-01-02 19:54:20 -0300

    criando pacotes

---
+++ tech_challenge_web_streaming/src/main/java/com/fiap/tech_challenge_web_streaming/controllers/Usuario.java
@@ -0,0 +1,4 @@
+package com.fiap.tech_challenge_web_streaming.controllers;
+
+public class Usuario {
+}
---
+++ tech_challenge_web_streaming/src/main/java/com/fiap/tech_challenge_web_streaming/usecases/Usuario.java
@@ -0,0 +1,4 @@
+package com.fiap.tech_challenge_web_streaming.usecases;
+
+public class Usuario {
+}
