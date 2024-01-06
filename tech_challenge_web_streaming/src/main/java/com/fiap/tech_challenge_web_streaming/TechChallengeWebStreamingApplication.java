fce51aea6b1471c27cf56dd5eef3a05443a09269
kassimentz <kassimentz@gmail.com>
2024-01-06 12:21:03 -0300

    configuring mongodb embeded

--- tech_challenge_web_streaming/src/main/java/com/fiap/tech_challenge_web_streaming/TechChallengeWebStreamingApplication.java
+++ tech_challenge_web_streaming/src/main/java/com/fiap/tech_challenge_web_streaming/TechChallengeWebStreamingApplication.java
@@ -2,8 +2,10 @@ package com.fiap.tech_challenge_web_streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class TechChallengeWebStreamingApplication {
 
 	public static void main(String[] args) {
