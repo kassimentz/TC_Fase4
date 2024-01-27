56de97256258c7e1c15abfc26f6cfbdac72b4de9
kmentz <kassiane.mentz@adp.com>
2024-01-27 19:29:09 -0300

    voltando DTO

---
+++ tech_challenge_web_streaming/src/main/java/com/fiap/tech_challenge_web_streaming/usecase/usuario/dto/IUsuarioUpdateData.java
@@ -0,0 +1,13 @@
+package com.fiap.tech_challenge_web_streaming.usecase.usuario.dto;
+
+import com.fiap.tech_challenge_web_streaming.domain.video.entity.Video;
+
+import java.util.List;
+
+public interface IUsuarioUpdateData {
+
+    String nome ();
+
+    String email ();
+
+}
