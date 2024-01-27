4cbd8170da05622ed3967f26cf67273f775758cb
kmentz <kassiane.mentz@adp.com>
2024-01-27 16:40:43 -0300

    corrigindo mensagens de erro de validacao

--- tech_challenge_web_streaming/src/main/java/com/fiap/tech_challenge_web_streaming/exception/GlobalExceptionHandler.java
+++ tech_challenge_web_streaming/src/main/java/com/fiap/tech_challenge_web_streaming/exception/GlobalExceptionHandler.java
@@ -1,69 +1,27 @@
 package com.fiap.tech_challenge_web_streaming.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConversionException;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.MissingRequestValueException;
+import org.springframework.web.context.request.WebRequest;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErroValidacaoResponse> handleValidationException(MethodArgumentNotValidException ex) {
-        ErroValidacaoResponse response = new ErroValidacaoResponse();
-        response.setStatus(400);
-        response.setMensagem("Erro de validação");
+    public ResponseEntity<Object> handleValidationExceptions(
+            MethodArgumentNotValidException ex, WebRequest request) {
 
-        List<String> erros = new ArrayList<>();
+        String errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
 
-        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
-            if (error instanceof FieldError) {
-                FieldError fieldError = (FieldError) error;
-                String mensagemErro = "Campo '" + fieldError.getField() + "': " + fieldError.getDefaultMessage();
-                erros.add(mensagemErro);
-            } else {
-                erros.add(error.getDefaultMessage());
-            }
-        }
-
-        response.setErros(erros);
-
-        return ResponseEntity.badRequest().body(response);
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
-
-    @ExceptionHandler(HttpMessageConversionException.class)
-    public ResponseEntity<ErrorMessage> handleHttpMessageConversionException(HttpMessageConversionException ex) {
-        String errorMessage = "Ocorreu um erro na desserialização do JSON. Verifique os tipos dos atributos.";
-        ErrorMessage error = new ErrorMessage(errorMessage);
-        return ResponseEntity.badRequest().body(error);
-    }
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorMessage> runtimeException(RuntimeException ex) {
-        String errorMessage = ex.getMessage();
-        ErrorMessage error = new ErrorMessage(errorMessage);
-        return ResponseEntity.badRequest().body(error);
-    }
-
-//    @ExceptionHandler(MissingServletRequestParameterException.class)
-//    public ResponseEntity<ErroValidacaoResponse> handleValidationParameterException(MissingServletRequestParameterException ex) {
-//        ErroValidacaoResponse response = new ErroValidacaoResponse();
-//        response.setStatus(400);
-//        response.setMensagem("Erro de validação");
-//
-//        List<String> erros = new ArrayList<>();
-//
-//        String mensagemErro = "Campo '" + ex.getParameterName() + "': " + ex.getMessage();
-//        erros.add(mensagemErro);
-//        response.setErros(erros);
-//
-//        return ResponseEntity.badRequest().body(response);
-//    }
 }
